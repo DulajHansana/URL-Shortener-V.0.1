6bfdc3699aec7e2ee2b60672ec733f9b54c1e1f9
DuLa_1sT <161925626+DulajHansana@users.noreply.github.com>
2025-01-29 14:50:38 +0530

    Add Linkresult component to App and create initial Linkresult file

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import Home from './component/home';
 import Background from './component/background';
+import Linkresult from './component/linkresult';
 
 function App() {
   return (
     <div className='container'>
       <Home />
       <Background />
+      <Linkresult />
     </div>
    
   );
