65c839398119efb56aa70b2b94a6c440c757d276
Dev.Dulaj <161925626+DulajHansana@users.noreply.github.com>
2025-02-05 23:32:03 +0530

    Remove unused input and button elements from Home component

--- front-end/src/component/home.jsx
+++ front-end/src/component/home.jsx
@@ -4,10 +4,7 @@ function home() {
   return (
     <div className='inputcontainer'>
       <h1>URL <span>Shortener</span></h1>
-      <input type="text" placeholder='Past the link to shorten it' />
-      <button>Shorten</button>
-      
-      
+      <input t
     </div>
   )
 }
