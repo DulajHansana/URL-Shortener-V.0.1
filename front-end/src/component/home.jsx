9de0c6eaf3e122a77a4d0b78a75f783e7212a014
Dev.Dulaj <161925626+DulajHansana@users.noreply.github.com>
2025-02-07 23:16:45 +0530

    Remove input and button elements from Home component

--- front-end/src/component/home.jsx
+++ front-end/src/component/home.jsx
@@ -2,14 +2,6 @@ import React from 'react'
 
 function home() {
   return (
-    <div className='inputcontainer'>
-      <h1>URL <span>Shortener</span></h1>
-      <input type="text" placeholder='Past the link to shorten it' />
-      <button>Shorten</button>
-      
-      
-    </div>
-  )
-}
+
 
 export default home
