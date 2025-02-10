04068287ba46ea74d94418d8ff61c79c4b98bf5f
Dev.Dulaj <161925626+DulajHansana@users.noreply.github.com>
2025-02-10 23:36:39 +0530

    Style LinkResult component and add copy button functionality

--- front-end/src/component/linkresult.jsx
+++ front-end/src/component/linkresult.jsx
@@ -3,9 +3,10 @@ import React, { useState } from 'react'
 const LinkResult = () => {
   const[shortenlink, setShortenlink] = useState("Hello World");
   return (
-    <div>
+    <div className='result'>
       <p>{shortenlink}</p>
-     
+      <button>Copy to clipboard</button>
+      
     </div>
   )
 }
