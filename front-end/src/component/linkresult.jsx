67350350e7ccb86913e90eb5927ecdead3e76551
Dev.Dulaj <161925626+DulajHansana@users.noreply.github.com>
2025-02-10 23:54:59 +0530

    Add copy to clipboard functionality and style for copied state in LinkResult component

--- front-end/src/component/linkresult.jsx
+++ front-end/src/component/linkresult.jsx
@@ -1,11 +1,18 @@
 import React, { useState } from 'react'
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 const LinkResult = () => {
   const[shortenlink, setShortenlink] = useState("Hello World");
+  const[copied, setCopied] = useState(false);
   return (
     <div className='result'>
       <p>{shortenlink}</p>
-      <button>Copy to clipboard</button>
+      <CopyToClipboard text={shortenlink}
+        onCopy={() => setCopied(true)}>
+
+      <button className={copied ? "copied": ""}>Copy to clipboard</button>
+        </CopyToClipboard>
+        
       
     </div>
   )
