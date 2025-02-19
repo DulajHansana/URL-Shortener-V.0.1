ccb3b225c1d36abe5392dd8e8d384d6bf809c1a7
Dev.Dulaj <161925626+DulajHansana@users.noreply.github.com>
2025-02-19 21:39:03 +0530

    Update Linkresult component to include copy-to-clipboard functionality and improve styling

--- front-end/src/component/linkresult.jsx
+++ front-end/src/component/linkresult.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import {useState} from 'react'
+import {CopyToClipboard} from 'react-copy-to-clipboard'
 
-function linkresult() {
+const Linkresult = () => {
+    const [linkesult, setLinkresult] = useState("Hello World");
+    const [copied, setCopied] = useState(false);
   return (
-    <div>
+    <div className='result'>
+        <p>{linkesult}</p>
+        
+        <CopyToClipboard text={linkesult}
+        onCopy={()=> setCopied(true)}>
+        <button className={copied ? "copied": ""}>Copy to clipboard</button>
+          </CopyToClipboard> 
+
+        
     </div>
   )
 }
 
-export default linkresult
+export default Linkresult
